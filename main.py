787443f1123eddbf0046c5557afeb4231c7661f3
michal <michalchelminski@gmail.com>
2021-10-09 16:40:58 +0200

    remove .env file

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
