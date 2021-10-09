614c2eb905be93c094a36c56a36475cdba6a9ed5
michal <michalchelminski@gmail.com>
2021-10-09 16:14:55 +0200

    Add .env file and change the way of accessing env variables to dotenv

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
